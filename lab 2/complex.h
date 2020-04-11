bdb0f1357d1860e333e69a46e9ccfd6e0c76f46c
EzhNEesh <62452675+EzhNEesh@users.noreply.github.com>
2020-04-11 12:52:27 +0300

    Update complex.h

--- lab 2/complex.h
+++ lab 2/complex.h
@@ -2,11 +2,12 @@
 class Complex
 {
 public:
-	Complex();
+	Complex(double re, double im);
+	Complex(Complex& com);
 	void create(double re, double im);
 	void mult_real(double re);
 	void add_complex(double re, double im);
-	void mult_complex(double re, double im);
+	void mult_complex(Complex B);
 	void print_length();
 	void print();
 private:
